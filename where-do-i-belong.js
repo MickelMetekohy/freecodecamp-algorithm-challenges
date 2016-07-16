c8d40950be6315dc5a7f24982bdf7a7d7582b15a
demikeison <m.star28@gmail.com>
2016-07-16 16:45:54 -0400

    update where do I belong

--- where-do-i-belong.js
+++ where-do-i-belong.js
@@ -30,10 +30,17 @@ getIndexToIns([2, 5, 10], 15);
  * @param Number
  * @return Number
  */
- function getIndexToIns(arr, num) {
-   // sort and find right index
-   var index = arr.sort((curr, next) => curr > next)
-     .findIndex((currNum)=> num <= currNum);
-   // Returns proper answer
-   return index === -1 ? arr.length : index;
- }
+function getIndexToIns(arr, num) {
+    var index = arr.sort((curr, next) => curr > next)
+        .findIndex((currNum) => num <= currNum);
+    return index === -1 ? arr.length : index;
+}
+
+
+
+getIndexToIns([10, 20, 30, 40, 50], 35); // 3.
+getIndexToIns([10, 20, 30, 40, 50], 30); // 2.
+getIndexToIns([40, 60], 50); // 1.
+getIndexToIns([3, 10, 5], 3); // 0.
+getIndexToIns([5, 3, 20, 3], 5); // 2.
+getIndexToIns([2, 20, 10], 19); // 2
