189c0f39d3f98ef343af7f9cf26c13c933701fc2
Mickel Metekohy <m.star28@gmail.com>
2016-11-29 21:39:57 -0500

    adds drop it

---
+++ drop-it.js
@@ -0,0 +1,19 @@
+/**
+ * Drop it
+ */
+function dropElements(arr, func) {
+  // Drop them elements.
+  var result = arr;
+  while(!func(result[0])) {
+    result.shift();
+  }
+  return result;
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n >= 3;}); //should return [3, 4].
+dropElements([0, 1, 0, 1], function(n) {return n === 1;}); //should return [1, 0, 1].
+dropElements([1, 2, 3], function(n) {return n > 0;}); //should return [1, 2, 3].
+dropElements([1, 2, 3, 4], function(n) {return n > 5;}); //should return [].
+dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}); //should return [7, 4].
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}); //should return [3, 9, 2]
+
