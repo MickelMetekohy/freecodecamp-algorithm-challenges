4ff9bdc25eac3df0f1b2e0a35c7500e4651a7fb5
Mickel Metekohy <m.star28@gmail.com>
2017-02-09 23:30:19 -0500

    adds dna pairing

---
+++ dna-pairing.js
@@ -0,0 +1,25 @@
+/**
+ * DNA Pairing
+ */
+
+/**
+ * DNA Pairing
+ *
+ * @param str
+ * @return Array
+ */
+function pairElement(str) {
+  var pairs = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
+  };
+  return str.split("").map(function(el) {
+    return [el,pairs[el]];
+  });
+}
+
+pairElement("ATCGA") // should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+pairElement("TTGAG") // should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+pairElement("CTCTA") // should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
